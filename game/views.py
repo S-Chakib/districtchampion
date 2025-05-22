78e78f0978eb9009720cc701e799bf7dfc8168d2
chakib <chakib@MacBook-Air.localdomain>
2025-05-22 22:36:00 +0200

    game_area

--- game/views.py
+++ game/views.py
@@ -7,27 +7,14 @@ def game(request):
     0: {"top": 20, "left": 10},
     1: {"top": 35, "left": 10},
     2: {"top": 38, "left": 8},
+    3: {"top": 52, "left": 10},
     4: {"top": 52, "left": 10},
     5: {"top": 55, "left": 8},
     6: {"top": 73, "left": 10},
-    7: {"top": 100, "left": 100},
-    8: {"top": 100, "left": 100},
-    9: {"top": 100, "left": 100},
-    10: {"top": 100, "left": 100},
-    11: {"top": 100, "left": 100},
-    12: {"top": 100, "left": 100},
-    13: {"top": 100, "left": 100},
-    14: {"top": 100, "left": 100},
-    15: {"top": 100, "left": 100},
-    16: {"top": 100, "left": 100},
-    17: {"top": 100, "left": 100},
-    18: {"top": 100, "left": 100},
-    19: {"top": 100, "left": 100},
-    20: {"top": 100, "left": 100},
-    21: {"top": 100, "left": 100},
+
 }
 
     return render(request, "game.html", {
         "card_positions": card_positions,
-        "card_range": range(22),
+        "card_range": range(7),
     })
